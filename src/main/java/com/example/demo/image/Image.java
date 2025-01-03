10c82a08597c5044773121c5ffe3508604281bfc
Aria <ariaman2002@gmail.com>
2025-01-03 11:35:04 +0330

    Set image field  in image class and return base64 string in service, set new GetMapping annotation for /api/v1/image api path.

--- src/main/java/com/example/demo/image/Image.java
+++ src/main/java/com/example/demo/image/Image.java
@@ -1,4 +1,25 @@
 package com.example.demo.image;
 
 public class Image {
+
+    private String image;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Image(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Image{" +
+                "image='" + image + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/example/demo/image/ImageController.java
+++ src/main/java/com/example/demo/image/ImageController.java
@@ -1,4 +1,21 @@
 package com.example.demo.image;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/image")
+@CrossOrigin("http://localhost:3000")
 public class ImageController {
+    private final ImageService imageService;
+
+    @Autowired
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
+
+    @GetMapping
+    public String getImage() {
+        return this.imageService.getImage();
+    }
 }
